818cdffc208741b0a56b8603ba8445747a87b0f9
Kasia <katarzyna.tramea@gmail.com>
2022-01-14 17:55:55 +0100

    update UserRepository.java

--- src/main/java/com/poecat/PainterAdminApp/repository/UserRepository.java
+++ src/main/java/com/poecat/PainterAdminApp/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.poecat.PainterAdminApp.repository;
 
-public class UserRepository {
-}
+import com.poecat.PainterAdminApp.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {}
