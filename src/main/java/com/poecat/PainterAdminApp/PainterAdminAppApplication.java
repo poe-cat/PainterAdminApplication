5027a12df2f0744f0a31caff7ca7fcad9850d3c4
Kasia <katarzyna.tramea@gmail.com>
2022-01-14 17:41:10 +0100

    first commit

---
+++ src/main/java/com/poecat/PainterAdminApp/PainterAdminAppApplication.java
@@ -0,0 +1,13 @@
+package com.poecat.PainterAdminApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainterAdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainterAdminAppApplication.class, args);
+	}
+
+}
