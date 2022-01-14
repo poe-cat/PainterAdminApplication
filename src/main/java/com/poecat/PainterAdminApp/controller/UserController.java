bad66d5601fdba62f4b878861276efce138126bf
Kasia <katarzyna.tramea@gmail.com>
2022-01-14 18:35:03 +0100

    create UserController.java & create add & sign up methods

---
+++ src/main/java/com/poecat/PainterAdminApp/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.poecat.PainterAdminApp.controller;
+
+import com.poecat.PainterAdminApp.model.User;
+import com.poecat.PainterAdminApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/signup")
+    public String showSignUpForm(User user) {
+        return "add-user";
+    }
+
+    @PostMapping("/adduser")
+    public String addUser(@Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+
+        userRepository.save(user);
+        return "redirect:/index";
+    }
+}
